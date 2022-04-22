12c652e7eea46c65ebdbb55ed0d58bbd2a761db6
YXL <chenxin.lan.76@gmail.com>
2022-04-22 09:34:56 +0800

    chore(submodule): Add `rust-gc`
    Signed-off-by: YXL <chenxin.lan.76@gmail.com>

--- src/port.rs
+++ src/port.rs
@@ -4,7 +4,7 @@ use {
     x86_64::instructions::port::{PortRead, PortReadOnly, PortWrite, PortWriteOnly},
 };
 
-fn port_in<T: PortRead + Into<i32>>(
+fn port_in<T: PortRead + Into<f64>>(
     _this: &JsValue,
     args: &[JsValue],
     context: &mut Context,
@@ -17,7 +17,7 @@ fn port_in<T: PortRead + Into<i32>>(
     let mut port = PortReadOnly::<T>::new(port);
     let value = unsafe { port.read() };
 
-    Ok(JsValue::Integer(value.into()))
+    Ok(JsValue::Rational(value.into()))
 }
 
 fn port_out<T: PortWrite + TryFrom<u32> + Default>(
@@ -49,20 +49,6 @@ pub fn init(obj: &mut ObjectInitializer) {
         .function(port_out::<u8>, "outb", 2)
         .function(port_in::<u16>, "inw", 1)
         .function(port_out::<u16>, "outw", 2)
-        .function(
-            |_this, args, context| {
-                let port = args
-                    .get(0)
-                    .ok_or(context.construct_type_error("missing port"))?
-                    .to_uint16(context)?;
-
-                let mut port = PortReadOnly::<u32>::new(port);
-                let value = unsafe { port.read() };
-
-                Ok(JsValue::Rational(value.into()))
-            },
-            "inl",
-            1,
-        )
+        .function(port_in::<u32>, "inl", 1)
         .function(port_out::<u32>, "outl", 2);
 }
